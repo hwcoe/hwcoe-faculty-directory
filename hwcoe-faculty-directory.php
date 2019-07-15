c811555f81b3c830835c2a3c2fe8649b33b62be4
sezj <sarahzachrich@gmail.com>
2019-07-15 14:11:57 -0400

    Add ACF field group for faculty post fields; add function to create faculty_pg category

--- hwcoe-faculty-directory.php
+++ hwcoe-faculty-directory.php
@@ -10,6 +10,7 @@ Author URI: http://allisoncandreva.com/
 
 
 /* Enqueue assets */
+// needed?
 add_action( 'wp_enqueue_scripts', 'hwcoe_faculty_directory_assets' );
 function hwcoe_faculty_directory_assets() {
 
@@ -18,14 +19,26 @@ function hwcoe_faculty_directory_assets() {
 /*
 * Adds Category for Faculty Post
 */
-function hwcoe_faculty_directory_insert_category() {
-	wp_insert_term(
-		'faculty-pg',
-		'category',
-		array(
-		  'description'	=> 'This category is only used for faculty posts.',
-		  'slug' 		=> 'faculty-pg'
-		)
-	);
+if ( !function_exists('hwcoe_faculty_directory_insert_category') ) {
+	function hwcoe_faculty_directory_insert_category() {
+		wp_insert_term(
+			'faculty-pg',
+			'category',
+			array(
+			  'description'	=> 'This category is only used for faculty posts.',
+			  'slug' 		=> 'faculty-pg'
+			)
+		);
+	}
 }
 add_action( 'after_setup_theme', 'hwcoe_faculty_directory_insert_category' );
+
+function hwcoe_faculty_directory_template() {
+	if ( in_category('faculty-pg') ) {
+		include 'single-post-faculty.php';
+		
+	// TODO: switch to faculty post template when faculty-pg category is selected	
+	}
+}
+add_filter( 'single_template', 'hwcoe_faculty_directory_template' );
+// TODO: load acf local json from plugin folder
